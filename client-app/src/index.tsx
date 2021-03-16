8e205db773e94c64a46687358d75c67f2bfe5361
Zethzer <bernard.y24@gmail.com>
2021-03-17 00:25:21 +0100

    End of Section 8

--- client-app/src/index.tsx
+++ client-app/src/index.tsx
@@ -5,14 +5,17 @@ import './app/layout/styles.css';
 import App from './app/layout/App';
 import reportWebVitals from './reportWebVitals';
 import { store, StoreContext } from './app/stores/store';
+import { BrowserRouter } from 'react-router-dom';
 
 ReactDOM.render(
-  // <React.StrictMode>
-  <StoreContext.Provider value={store}>
-    <App />
-  </StoreContext.Provider>,
-  // </React.StrictMode>,
-  document.getElementById('root')
+    // <React.StrictMode>
+    <StoreContext.Provider value={store}>
+        <BrowserRouter>
+            <App />
+        </BrowserRouter>
+    </StoreContext.Provider>,
+    // </React.StrictMode>,
+    document.getElementById('root')
 );
 
 // If you want to start measuring performance in your app, pass a function
