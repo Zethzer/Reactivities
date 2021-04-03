b1f366a27494438451f7a435d3f921a6f3c32248
Zethzer <bernard.y24@gmail.com>
2021-04-03 02:39:38 +0200

    Hash for FF

--- API/Startup.cs
+++ API/Startup.cs
@@ -62,7 +62,7 @@ namespace API
                 .FormActions(s => s.Self())
                 .FrameAncestors(s => s.Self())
                 .ImageSources(s => s.Self().CustomSources("https://res.cloudinary.com/"))
-                .ScriptSources(s => s.Self().CustomSources("sha256-MaOASONmzK1ZXxdI05yW7dB2tGOBfaXkEilKZU5cgWU=","sha256-FlmTAbVnnzjiprkXdkS9e+vyjb5BFl+4V8tOsgxVm60="))
+                .ScriptSources(s => s.Self().CustomSources("sha256-MaOASONmzK1ZXxdI05yW7dB2tGOBfaXkEilKZU5cgWU=","sha256-RRtI1mKbaJT8CmPkWm5fHb2In1iU2aKwh/MwSLSFSIs="))
             );
             
             if (env.IsDevelopment())
